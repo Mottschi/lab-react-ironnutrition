2b9d659a7002171b4d6848f21632fedb9cc0adc2
Sebastian Mottschall <srophkor@gmail.com>
2023-03-04 12:46:04 +0100

    removing some trial code from testing controlled number input

--- src/components/AddFoodForm.jsx
+++ src/components/AddFoodForm.jsx
@@ -2,87 +2,119 @@ import { Divider, Input } from 'antd';
 import { useState } from 'react';
 
 // Iteration 4
-function AddFoodForm({handleCreateFood}) {
-    const [name, setName] = useState('');
-    const [image, setImage] = useState('');
-    const [calories, setCalories] = useState('');
-    const [servings, setServings] = useState('');
+function AddFoodForm({ handleCreateFood }) {
+  const [name, setName] = useState('');
+  const [image, setImage] = useState('');
+  const [calories, setCalories] = useState('');
+  const [servings, setServings] = useState('');
 
-    function handleNameInputChange(event) {
-        setName(event.target.value)
+  function handleNameInputChange(event) {
+    setName(event.target.value);
+  }
+
+  function handleImageInputChange(event) {
+    setImage(event.target.value);
+  }
+
+  function handleCaloriesInputChange(event) {
+    // There is some weirdness with invalid inputs with number inputs
+    // this conditional if block helps us avoid part of them - it blocks
+    // inputs that start with a number, but then have letters follow
+    if (!event.target.checkValidity()) {
+      return;
     }
 
-    function handleImageInputChange(event) {
-        setImage(event.target.value)
+    // this conditional makes sure that when a number was entered, it is possible
+    // to delete it with delete/backspace
+    if (event.target.value === '') {
+      setCalories('');
+      return;
     }
 
-    function handleCaloriesInputChange(event) {
-        // There is some weirdness with invalid inputs with number inputs
-        // this conditional if block helps us avoid part of them - it blocks
-        // inputs that start with a number, but then have letters follow
-        if (!event.target.checkValidity()) {
-          event.target.value = calories;
-          return;
-        }
+    let cal = parseInt(event.target.value);
+    if (!isNaN(cal)) setCalories(cal);
+  }
 
-        // this conditional makes sure that when a number was entered, it is possible
-        // to delete it with delete/backspace
-        if (event.target.value === ''){
-          setCalories('');
-          return;
-        } 
-
-        let cal = parseInt(event.target.value);
-        if (!isNaN(cal)) setCalories(cal);
+  function handleServingsInputChange(event) {
+    console.log('change trigger');
+    // There is some weirdness with invalid inputs with number inputs
+    // this conditional if block helps us avoid part of them - it blocks
+    // inputs that start with a number, but then have letters follow
+    if (!event.target.checkValidity()) {
+      return;
     }
 
-    function handleServingsInputChange(event) {
-        // There is some weirdness with invalid inputs with number inputs
-        // this conditional if block helps us avoid part of them - it blocks
-        // inputs that start with a number, but then have letters follow
-        if (!event.target.checkValidity()) {
-          event.target.value = servings;
-          return;
-        }
-
-        // this conditional makes sure that when a number was entered, it is possible
-        // to delete it with delete/backspace
-        if (event.target.value === '') {
-          setServings('');
-          return;}
-        
-        const serve = parseInt(event.target.value);
-        if (!isNaN(serve)) setServings(serve);
+    // this conditional makes sure that when a number was entered, it is possible
+    // to delete it with delete/backspace
+    if (event.target.value === '') {
+      setServings('');
+      return;
     }
+    const serve = parseInt(event.target.value);
+    if (!isNaN(serve)) setServings(serve);
+  }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (
+      !name.length ||
+      !image.length ||
+      typeof calories === 'string' ||
+      typeof servings === 'string'
+    )
+      return alert('Please fill out all fields before submitting a new food!');
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        if (!name.length || !image.length || typeof calories === 'string' || typeof servings === 'string') return alert('Please fill out all fields before submitting a new food!');
-
-        const newFood = {name, image, servings, calories};
-        handleCreateFood(newFood);
-        setName('');
-        setImage('');
-        setCalories('');
-        setServings('');
-    }
+    const newFood = { name, image, servings, calories };
+    handleCreateFood(newFood);
+    setName('');
+    setImage('');
+    setCalories('');
+    setServings('');
+  }
 
   return (
     <form onSubmit={handleSubmit}>
       <Divider>Add Food Entry</Divider>
 
       <label>Name:</label>
-      <Input value={name} type="text" placeholder="Name" onChange={handleNameInputChange} required/>
+      <Input
+        value={name}
+        type="text"
+        placeholder="Name"
+        onChange={handleNameInputChange}
+        required
+      />
 
-      <label htmlFor='input-image'>Image:</label>
-      <Input type='text' name='image' id='input-image' value={image} placeholder="Image URL" onChange={handleImageInputChange} required/>
+      <label htmlFor="input-image">Image:</label>
+      <Input
+        type="text"
+        name="image"
+        id="input-image"
+        value={image}
+        placeholder="Image URL"
+        onChange={handleImageInputChange}
+        required
+      />
 
       <label>Calories:</label>
-      <Input type='number' name='calories' id='input-calories' value={calories} placeholder="Calories" onChange={handleCaloriesInputChange}/>
+      <Input
+        type="number"
+        name="calories"
+        id="input-calories"
+        value={calories}
+        placeholder="Calories"
+        onChange={handleCaloriesInputChange}
+      />
 
       <label>Servings:</label>
-      <Input type='number' name='calories' id='input-servings' value={servings} placeholder="Servings" onChange={handleServingsInputChange}/>
+      <Input
+        type="number"
+        name="calories"
+        id="input-servings"
+        value={servings}
+        placeholder="Servings"
+        onChange={handleServingsInputChange}
+      />
 
       <button type="submit">Create</button>
     </form>
